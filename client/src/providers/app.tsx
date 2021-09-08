50db649bc50bc003781543c39aa085943afd8fbf
Eric Do <ericdo.617@gmail.com>
2021-09-08 16:41:41 -0700

    Remove hash router

--- client/src/providers/app.tsx
+++ client/src/providers/app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {ChakraProvider } from "@chakra-ui/react";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from "lib/react-query";
 import theme from 'theme';
