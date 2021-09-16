4394e509d3ec9a7cbd808dcb52f574a70f735d2a
Eric Do <ericdo.617@gmail.com>
2021-09-15 19:09:06 -0700

    Add forms to login/registration

--- client/src/features/auth/login.tsx
+++ client/src/features/auth/login.tsx
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  Button,
   FormControl,
   FormLabel,
-  Input
+  Input,
+  Stack
 } from '@chakra-ui/react'
 
+interface LoginInput {
+  username: string;
+  password: string;
+}
+
+const defaultInput = {
+  username: '',
+  password: ''
+}
+
 export const Login = () => {
+  const [input, setInput] = useState<LoginInput>(defaultInput)
+  const { username, password } = input;
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+    setInput({
+      ...input,
+      [name]: value
+    })
+  }
+
   return (
     <form>
-      <FormControl id="username" isRequired>
-        <FormLabel>Username</FormLabel>
-        <Input placeholder="Username" />
-      </FormControl>
-      <FormControl id="password" isRequired>
-        <FormLabel>Password</FormLabel>
-        <Input placeholder="Password" />
-      </FormControl>
+      <Stack spacing={4}>
+        <FormControl id="username" isRequired>
+          <FormLabel>Username</FormLabel>
+          <Input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleChange}
+          />
+        </FormControl>
+        <FormControl id="password" isRequired>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </FormControl>
+      </Stack>
+      <Button
+        mt={10}
+        colorScheme="teal"
+        type="submit"
+      >
+        Submit
+      </Button>
     </form>
   )
 };
