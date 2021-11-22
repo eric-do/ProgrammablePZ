07da2a5342f5d5ceeaccbc74d5b62ed74aece97a
Eric Do <ericdo.617@gmail.com>
2021-11-22 14:43:34 -0800

    Implement debounce

--- client/src/test/server/handlers/friendships.ts
+++ client/src/test/server/handlers/friendships.ts
@@ -1,15 +1,24 @@
 import { API_URL } from 'config';
 import { rest } from 'msw'
-import { generateListOfFriends } from '../../data-generators';
+import {
+  generateListOfFriends,
+  generateFriendshipMetadata
+} from '../../data-generators';
 
-export const userHandlers = [
+export const friendshipHandlers = [
   rest.get(`${API_URL}/api/friendships/friends`,
     (req, res, ctx) => {
+      console.log('GETTING FRIENDS')
       return res(ctx.json(generateListOfFriends()))
   }),
 
   rest.post(`${API_URL}/api/friendships/create`,
     (req, res, ctx) => {
       return res(ctx.status(201))
+  }),
+
+  rest.get(`${API_URL}/api/friendships/metadata`,
+    (req, res, ctx) => {
+      return res(ctx.json(generateFriendshipMetadata()))
   }),
 ]
--- client/src/test/server/handlers/index.ts
+++ client/src/test/server/handlers/index.ts
@@ -1,9 +1,11 @@
 import { ridesHandlers } from "./rides";
 import { authHandlers } from "./auth";
 import { userHandlers } from "./user";
+import { friendshipHandlers } from "./friendships";
 
 export const handlers = [
   ...ridesHandlers,
   ...authHandlers,
-  ...userHandlers
+  ...userHandlers,
+  ...friendshipHandlers
 ];
