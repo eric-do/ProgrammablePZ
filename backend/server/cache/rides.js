10d113e239c2366ec58f12a15f3bd6943b977e15
Eric Do <ericdo.617@gmail.com>
2021-11-24 15:53:34 -0800

    Add caching to timeline

--- backend/server/cache/rides.js
+++ backend/server/cache/rides.js
@@ -13,57 +13,7 @@ const clearRideFromCache = (req, res, next) => {
   next();
 }
 
-const keyGenerator = (listType, filters) => {
-  const filterKeyValues = Object.entries(filters)
-                            .map(([k, v]) => `${k}_${v}`)
-                            .join('_');
-
-  return `${listType}_${filterKeyValues}`
-}
-
-const getCachedRides = (req, res, next) => {
-  const key = keyGenerator('popularRides', req.query)
-
-  redis.get(key, (err, rides) => {
-    if (err) {
-      console.log(err)
-      next('route');
-    } else if (!rides) {
-      next('route');
-    } else {
-      const parsedRides = JSON.parse(rides);
-      res.locals.data = parsedRides;
-      next();
-    }
-  })
-}
-
-const setCachedRides = (req, res, next) => {
-  const {
-    user,
-    type,
-    timeInSeconds,
-    limit = 10
-  } = req.query;
-
-  const key = keyGenerator('popularRides', req.query)
-  const timeToExpire = 60 * 60 * 1;
-
-  redis.set(
-    key,
-    JSON.stringify(res.locals.data), 'EX', timeToExpire, (err, res) => {
-      if (err) {
-        console.log(err)
-        next();
-      } else {
-        next();
-      }
-  });
-}
-
 module.exports = {
   addRideToCache,
-  clearRideFromCache,
-  getCachedRides,
-  setCachedRides
+  clearRideFromCache
 }
--- backend/server/routes/timeline.js
+++ backend/server/routes/timeline.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const responseHandler = require('../middleware/responseHandler');
+const apicache = require('../lib/apicache').middleware;
+const { setTimelineCacheConfig } = require('../cache/timeline');
 const { validateToken } = require('../middleware/auth');
 const { getTimeline } = require('../controllers/timeline');
 const rideFormatter = require('../middleware/rideFormatter')
@@ -8,6 +10,8 @@ const rideFormatter = require('../middleware/rideFormatter')
 router.get(
   '/',
   validateToken,
+  apicache('1 hour'),
+  setTimelineCacheConfig,
   getTimeline,
   rideFormatter,
   responseHandler
