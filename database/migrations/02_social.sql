a19ff6aaaa4edc090f486de07a4c9a1546ee02da
Eric Do <ericdo.617@gmail.com>
2021-12-01 22:26:27 -0800

    Set up dockerization of backend

---
+++ database/migrations/02_social.sql
@@ -0,0 +1,15 @@
+\c ppz
+DROP TABLE IF EXISTS user_follows;
+
+CREATE TABLE user_follows (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  user_id uuid REFERENCES users (id) ON DELETE CASCADE,
+  friend_id uuid REFERENCES users (id) ON DELETE CASCADE,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  PRIMARY KEY (user_id, friend_id)
+);
+
+CREATE INDEX user_follows_userid_index ON user_follows (user_id);
+CREATE INDEX user_follows_friendid_index ON user_follows (friend_id);
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON user_follows TO postgres;
