67576cbc6d02080cfdfde5683bb6aec28fcca670
Eric Do <ericdo.617@gmail.com>
2021-12-16 10:39:38 -0800

    Add stress test script

--- performance/rides_stress_test.js
+++ performance/rides_stress_test.js
@@ -1,14 +1,14 @@
+// run with 'k6 run rides.js'
 import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
   stages: [
     { duration: '1m', target: 100 }, // below normal load
-    { duration: '1m', target: 200 },
-    { duration: '1m', target: 400 }, // normal load
-    { duration: '1m', target: 600 },
-    { duration: '1m', target: 800 }, // around the breaking point
-    { duration: '1m', target: 1000 }
+    { duration: '1m', target: 500 },
+    { duration: '1m', target: 1000 }, // normal load
+    { duration: '1m', target: 2000 },
+    { duration: '1m', target: 2500 }, // around the breaking point
   ],
 };
 
